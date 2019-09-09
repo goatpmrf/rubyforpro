ed24eb68ccd1b7826575d4b9574233f0c7a56491
goatpmrf <yuki.n708.sugawara@gmail.com>
2019-09-09 22:16:43 +0900

    9/9勉強分

---
+++ Section2/2_2.rb
@@ -0,0 +1,22 @@
+# 数値の１を文字列に変換する(かっこあり)
+
+a = 1.to_s()
+puts a
+
+# かっこなし
+b = 1.to_s
+puts b
+
+# 数値を１６進数の文字列に変換する
+c = 10.to_s(16)
+puts c
+
+# かっこなし
+d = 10.to_s 16
+puts d
+
+# セミコロンを使って１行に押し込める
+e = 1.to_s;puts e;d = nil.to_s;puts d;e = 10.to_s 16;puts e
+
+# 複数行コメントアウト
+# Ctrl + /
---
+++ Section2/2_3.rb
@@ -0,0 +1,13 @@
+# 文字列の比較
+
+a =  "ruby" == "ruby"
+puts a
+
+b =  "ruby" == "Ruby"
+puts b
+
+c =  "ruby" != "ruby"
+puts c
+
+d =  "ruby" != "Ruby"
+puts d
