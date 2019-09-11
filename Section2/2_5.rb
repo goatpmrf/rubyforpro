39194c75c943d5c400fffdb88a03beb8e13d86cc
goatpmrf <yuki.n708.sugawara@gmail.com>
2019-09-11 23:31:00 +0900

    35ページまで

---
+++ Section2/2_5.rb
@@ -0,0 +1,18 @@
+# if文あれこれ
+
+country = 'italy'
+
+# if文は、最後に評価された式を戻り値とする
+
+greeting =
+if country == 'japan'
+  'こんにちは'
+elsif country == 'us'
+  'Hello'
+elsif country == 'italy'
+  'ciao'
+else
+  '???'
+end
+
+puts greeting
