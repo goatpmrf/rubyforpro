397fcb31cd3c0e65a8d198853080db11ba73eb8a
goatpmrf <yuki.n708.sugawara@gmail.com>
2019-09-14 21:42:17 +0900

    rubocopで自動修正 コメントの位置を修正(2_5)

--- Section2/2_5.rb
+++ Section2/2_5.rb
@@ -2,17 +2,16 @@
 
 country = 'italy'
 
-# if文は、最後に評価された式を戻り値とする
-
 greeting =
-if country == 'japan'
+  if country == 'japan'
     'こんにちは'
-elsif country == 'us'
-  'Hello'
-elsif country == 'italy'
-  'ciao'
-else
-  '???'
-end
+  elsif country == 'us'
+    'Hello'
+  elsif country == 'italy'
+    'ciao'
+  else
+    '???'
+  end
 
+# if文は、最後に評価された式を戻り値とするので、greetingに代入されている
 puts greeting
