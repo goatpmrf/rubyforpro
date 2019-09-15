d6fb6a0f9861f67c5a0052e1726c90f3adde3a99
goatpmrf <yuki.n708.sugawara@gmail.com>
2019-09-16 01:43:14 +0900

    FizzBuzzのテストコード作成

--- test/fizz_buzz_test.rb
+++ test/fizz_buzz_test.rb
@@ -1,16 +1,5 @@
-def fizz_buzz(n)
-  if n % 15 == 0
-    'Fizz Buzz'
-  elsif n % 3 == 0
-    'Fizz'
-  elsif n % 5 ==0
-    'Buzz'
-  else
-    n.to_s
-  end
-end
-
 require 'minitest/autorun'
+require '../Section2/fizz_buzz'
 
 class FizzBuzzTest < Minitest::Test
   def test_fizz_buzz
@@ -23,12 +12,3 @@ class FizzBuzzTest < Minitest::Test
     assert_equal 'Fizz Buzz', fizz_buzz(15)
   end
 end
-
-# ループで書きたい
-# puts fizz_buzz(1)
-# puts fizz_buzz(2)
-# puts fizz_buzz(3)
-# puts fizz_buzz(4)
-# puts fizz_buzz(5)
-# puts fizz_buzz(6)
-# puts fizz_buzz(15)
