8bd5de78eb394eabeb4fef35a8b3e85f2d66dc46
goatpmrf <yuki.n708.sugawara@gmail.com>
2019-10-14 16:32:38 +0900

    7.3.3をアクセサメソッド２つに増やした

--- Section7/7_3_3_2.rb
+++ Section7/7_3_3_2.rb
@@ -1,20 +1,22 @@
 class User
   # @nameを読み書きするメソッドが自動的に定義される
-  attr_accessor :name
+  attr_accessor :name, :age
   # attr_reader :name
   # attr_writer :name
 
-  def initialize(name)
+  def initialize(name, age)
   @name = name
+  @age = age
   end
 
   # nameメソッドやname=メソッドを明示的に定義する必要がない
 end
 
-user = User.new('Alice')
+user = User.new('Alice', 20)
 
-# @nameを変更する
-user.name = 'Bob'
+# # @nameを変更する
+# user.name = 'Bob'
 
 # @nameを参照する
 p user.name
+p user.age
